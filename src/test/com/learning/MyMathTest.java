df2f0cb4a2c0fb58ea17ce512f2672ae0a7ac8ba
Tamminedi <vtammine@iMac.local>
2020-11-13 19:53:56 -0500

    First Code and First Unit Test

--- src/test/com/learning/MyMathTest.java
+++ src/test/com/learning/MyMathTest.java
@@ -2,10 +2,20 @@ package com.learning;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyMathTest {
 
+    MyMath myMath = new MyMath();
     @Test
-    public void test() {
+    public void sum_with3numbers() {
+        int result = myMath.sum(new int[]{1,2,3});
+        assertEquals(6, result);
+    }
 
+    @Test
+    public void sum_with1numbers() {
+        int result = myMath.sum(new int[]{3});
+        assertEquals(3, result);
     }
 }
